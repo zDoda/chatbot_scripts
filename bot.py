8b89fc83e5193c3e441ca7fa4a18bff3c573510a
czook <czook@iastate.edu>
2023-10-10 19:33:43 -0400

    working slack slash cmd

--- bot.py
+++ bot.py
@@ -1,70 +1,114 @@
 import slack
 import os
-from flask import Flask
+from flask import Flask, request, Response, jsonify
 from slackeventsapi import SlackEventAdapter
 import requests
 import json
+from threading import Thread
+import asyncio
 
 client = slack.WebClient(token=os.environ['SLACK_API_KEY'])
 app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'],'/slack/events',app)
 BOT_ID = client.api_call("auth.test")['user_id']
 user_conversation = []
-@slack_event_adapter.on('message')
-def message(payload):
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-    if user_id != BOT_ID:
-        url = f"https://general-runtime.voiceflow.com/state/user/{user_id}/interact?logs=off"
-        headers={
-            "Authorization": os.environ["VF_API_KEY"],
-            "accept": "application/json",
-            "content-type": "application/json",
-        }
-        #response = requests.delete(url,headers={"Authorization": os.environ["VF_API_KEY"]})
-        if user_id not in user_conversation:
-            payload_vf = {
-                "action": { "type": "launch" },
-                "config": {
-                    "tts": False,
-                    "stripSSML": True,
-                    "stopAll": True,
-                    "excludeTypes": ["block", "debug", "flow"]
-                }
-            }
-            response = requests.post(url=url, json=payload_vf, headers=headers)
-            user_conversation.append(user_id)
-            resp_dir = json.loads(response.text)
-            resp_text = resp_dir[0]['payload']['message']
-            print(response.text)
-        else:
-            payload_vf = {
-                "action": {
-                    "type": "text",
-                    "payload": text
-                },
-                "config": {
-                    "tts": False,
-                    "stripSSML": True,
-                    "stopAll": True,
-                    "excludeTypes": ["block", "debug", "flow"]
-                }
-            }
-            response = requests.post(url=url, json=payload_vf, headers=headers)
-            resp_dir = json.loads(response.text)
-            resp_text = ""
-            if resp_dir[-1]['type'] == "text":
-                resp_text = resp_dir[-1]['payload']['message']
-            print(response.text)
-        #launch message
-        client.chat_postMessage(channel=channel_id, text=resp_text)
-        #print(resp_text)
-        resp_dir = {}
-        resp_text = ""
+
+# @slack_event_adapter.on('message')
+# def message(payload):
+#     event = payload.get('event', {})
+#     channel_id = event.get('channel')
+#     user_id = event.get('user')
+#     text = event.get('text')
+#     if user_id != BOT_ID:
+#         url = f"https://general-runtime.voiceflow.com/state/user/{user_id}/interact?logs=off"
+#         headers={
+#             "Authorization": os.environ["VF_API_KEY"],
+#             "accept": "application/json",
+#             "content-type": "application/json",
+#         }
+#         #response = requests.delete(url,headers={"Authorization": os.environ["VF_API_KEY"]})
+#         if user_id not in user_conversation:
+#             payload_vf = {
+#                 "action": { "type": "launch" },
+#                 "config": {
+#                     "tts": False,
+#                     "stripSSML": True,
+#                     "stopAll": True,
+#                     "excludeTypes": ["block", "debug", "flow"]
+#                 }
+#             }
+#             response = requests.post(url=url, json=payload_vf, headers=headers)
+#             user_conversation.append(user_id)
+#             resp_dir = json.loads(response.text)
+#             resp_text = resp_dir[0]['payload']['message']
+#             print(response.text)
+#
+#         else:
+#             payload_vf = {
+#                 "action": {
+#                     "type": "text",
+#                     "payload": text
+#                 }, "config": {
+#                     "tts": False,
+#                     "stripSSML": True,
+#                     "stopAll": True,
+#                     "excludeTypes": ["block", "debug", "flow"]
+#                 }
+#             }
+#             response = requests.post(url=url, json=payload_vf, headers=headers)
+#             resp_dir = json.loads(response.text)
+#             resp_text = ""
+#             if resp_dir[-1]['type'] == "text":
+#                 resp_text = resp_dir[-1]['payload']['message']
+#             print(response.text)
+#
+#         #launch message
+#         client.chat_postMessage(channel=channel_id, text=resp_text)
+#         #print(resp_text)
+#         resp_dir = {}
+#         resp_text = ""
 
 
+# Ask a question
+@app.route('/q', methods=['POST'])
+def question():
+    data = request.form
 
+    thr = Thread(target=lambda: asyncio.run(task(data)))
+    thr.start()
+
+    # Respond to Slack immediately
+    example = {
+        'response_type': 'in_channel',
+        'text': 'Working on it. I am searching for your answer...',
+    }
+    return jsonify(example), 200
+
+async def task(data):
+    channel_id = data.get('channel_id')
+    text = data.get('text')
+
+    url = "https://general-runtime.voiceflow.com/knowledge-base/query"
+    payload = {
+        "chunkLimit": 4,
+        "synthesis": True,
+        "settings": {
+            "model": "gpt-3.5-turbo",
+            "temperature": 0.1,
+            "system": "You are an AI FAQ assistant. Information will be provided to help answer the user's questions. Always summarize your response to be as brief as possible and be extremely concise. Your responses should be fewer than a couple of sentences. Do not reference the material provided in your response."
+        },
+        "question": f'{text}'
+    }
+    headers={
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": os.environ["VF_API_KEY"]
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    temp = json.loads(response.text)
+    if temp['output'] is None:
+        client.chat_postMessage(channel=channel_id, text="Sorry I can't answer that question, please ask your manager")
+    else:
+        client.chat_postMessage(channel=channel_id, text=response.json()['output'])
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
---
+++ pricing.py
@@ -0,0 +1,93 @@
+
+pricing_dir = {
+    "fixe" : {
+        'TP' : [1.30, 2.36],
+        'P' : [3.12, 5.67],
+        'M' : [4.59, 8.35],
+        'G' : [6.21, 11.29],
+        'TG' : [7.68, 13.97]
+    },
+    "manivelle" : {
+        'P' : [4.59, 8.35],
+        'M' : [6.21, 11.29],
+        'G' : [7.68, 13.97],
+        'TG' : [8.66, 15.75]
+    },
+    "guillotine_simple" : {
+        'P' : [6.21, 11.29],
+        'M' : [7.68, 13.97],
+        'G' : [9.04, 16.43],
+    },
+    "guillotine_double" : {
+        'P' : [6.21, 19.58],
+        'M' : [7.68, 22.19],
+        'G' : [9.04, 24.80],
+    },
+    "couissante_simple" : {
+        'TP' : [3.95, 7.17],
+        'P' : [6.21, 9.40],
+        'M' : [7.69, 12.86],
+        'G' : [9.04, 14.33],
+        'TG' : [9.82, 17.85]
+    },
+    "couissante_simple" : {
+        'TP' : [3.95, 8.93],
+        'P' : [6.21, 11.55],
+        'M' : [7.69, 14.70],
+        'G' : [9.04, 17.85],
+        'TG' : [9.82, 23.10]
+    },
+    "francaise" : [.58, 1.05],
+    "puit_de_lumiere" : {
+        'P' : [5.25, 9.45],
+        'M' : [6.30, 15.75],
+        'G' : [7.35, 18.90],
+    },
+    "porte_coulissante" : {
+        'S' : [9.82, 17.85],
+        'D' : [9.82, 28.66]
+    },
+    "vider_les_gouttiere" : {
+        'Facile' : 2.26,
+        'Moyen' : 3.36,
+        'Difficile' : 4.67
+    },
+    "laver_soffites": 4.73,
+    "installation_de_plaques": 13.86,
+    "ampoules" : {
+        "rdc" : 3.68,
+        "etage" : 7.35
+    },
+    "nettoyage_de_lumiere" : 10.50,
+    "lavage_a_pression": {
+        "sols": {
+            "asphalte": .26,
+            "beton": .16
+        },
+        "murs": .13,
+        "bois": {
+            'sols': .42,
+            'cloture': .42,
+            'escalier': .42,
+            'rampes': 3.15
+        },
+        "pave_seulment": .58,
+        "pave_sable_g": 1.42,
+        "pave_sable_p": 1.68
+    },
+    "nettoyage_de_murs": {
+        'P': 47.25,
+        'M': 63.00,
+        'G': 89.25
+    },
+    "nettoyage_de_stores": {
+        'P': 15.75,
+        'M': 21.00,
+        'G': 26.25
+    },
+    "travaux_vip_heure": 126.00,
+    "tax": {
+        'tps': 0.05,
+        'tvq': 0.09975
+    }
+}
